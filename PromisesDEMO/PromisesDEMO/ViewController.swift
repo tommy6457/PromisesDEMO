2365ffcf73657a2b8bf0b1c766250988a57600cf
FETC_iMac2 <TommySTsai@fetc.net.tw>
2022-05-20 13:06:47 +0800

    import Promises SDK

--- PromisesDEMO/PromisesDEMO/ViewController.swift
+++ PromisesDEMO/PromisesDEMO/ViewController.swift
@@ -6,13 +6,73 @@
 //
 
 import UIKit
+import Promises
+
+enum TestError: Error {
+    case testerror
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        promiseTest(success: false)
+
     }
+
+    func promiseTest(success: Bool){
+
+        let promise = Promise<String>(on: .main) { fulfill, reject in
+
+            if success {
+                // Resolve with a value.
+                fulfill("Hello world.")
+            } else {
+                // Resolve with an error.
+                reject(TestError.testerror)
+            }
+
+        }
+
+        promise.then { string in
+            print("\(string)")  //Hello world.
+        }.catch { error in
+            print(error.self)   //testerror
+        }
+
+    }
+
+    func work1(_ string: String) -> Promise<String> {
+      return Promise {
+        return string
+      }
+    }
+
+    func work2(_ string: String) -> Promise<Int> {
+        return Promise {
+            return Int(string) ?? 0
+      }
+    }
+
+    func work3(_ number: Int) -> Int {
+      return number * number
+    }
+
+    func work4(){
+
+        work1("123zz").then { string in
+            return self.work2(string)
+        }.then { number in
+            return self.work3(number)
+        }.then { number in
+          print(number)  // 100
+        }.catch { error in
+            print("error:\(error.localizedDescription)")
+        }
+
+    }
+
 
 
 }
